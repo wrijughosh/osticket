3ae8d782f0cb6aba23832689455bf93664ff463a
Wriju Ghosh <wriju_ghosh@hotmail.com>
2018-01-10 15:16:16 +0530

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,9 +39,9 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','osticketmysql.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
+define('DBUSER','demouser@osticketmysql');
 define('DBPASS','demo@pass123');
 
 # Table prefix
